c41d01273693d34b5d454c329be7aba9d1a8ac08
XavierPainchaud <xav.painchaud@outlook.com>
2025-09-10 12:58:00 -0400

    Step 3 completed and add launch.json to debugg

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,37 @@
-// Vous devez insérer les nouveaux tests ici
-import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest';
+import app from '../../src/app';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+  //Initialiser joueur 1 et 2
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+  await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom2 });
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("devrait répondre à un appel réussi et le réponse devrait être en JSON", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  it("devrait répondre à une mauvaise demande pour des joueurs supprimés", async () => {
+    // Jouer avec des joueurs supprimer ne devrait pas fonctionner
+    const response = await request.get('/api/v1/jeu/jouer/' + testNom1);
+    expect(response.status).toBe(404);
+    expect(response.type).toBe("application/json");
+    expect(response.body.error).toInclude("n'existe pas");
+    expect(response.body.error).toInclude(testNom1);
+    
+    const response2 = await request.get('/api/v1/jeu/jouer/' + testNom2);
+    expect(response2.status).toBe(404);
+    expect(response2.type).toBe("application/json");
+    expect(response2.body.error).toInclude("n'existe pas");
+    expect(response2.body.error).toInclude(testNom2);
   });
 });
